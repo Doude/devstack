a7d06e703ca5e12043a6f3685dcfd603e29f3e09
Édouard Thuleau <edouard.thuleau@cloudwatt.com>
2014-04-25 17:35:51 +0200

    Corrector agent flag

--- contrail/contrail_config_templates.py
+++ contrail/contrail_config_templates.py
@@ -234,7 +234,7 @@ kmod=vrouter/vrouter.ko
 pname=vnswad
 LIBDIR=/usr/lib64
 VHOST_CFG=/etc/sysconfig/network-scripts/ifcfg-vhost0
-VROUTER_LOGFILE=--log-file=/var/log/vrouter.log
+VROUTER_LOGFILE=--DEFAULT.log_file=/var/log/vrouter.log
 COLLECTOR=$__contrail_collector__
 $__contrail_dev__
 """)
